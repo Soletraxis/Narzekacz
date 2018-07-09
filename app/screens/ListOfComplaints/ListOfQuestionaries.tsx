cd0a6f56c88e1725cb2fe45753a8a93344f2b769
Przemysław Przebieglec <p.przebieglec@gmail.com>
2018-07-09 12:41:42 +0200

    Add styling to ListOfComplaints

--- app/screens/ListOfComplaints/ListOfQuestionaries.tsx
+++ app/screens/ListOfComplaints/ListOfQuestionaries.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import {View, FlatList, Image} from "react-native";
 import Questionary from "./Questionary";
+import styles from "./styles";
 
 export default class ListOfQuestionaries extends Component<{item :object}, {}> {
     state = {
@@ -21,14 +22,19 @@ export default class ListOfQuestionaries extends Component<{item :object}, {}> {
     }
     render() {
         const {item} = this.props;
-        const uri = item.uri !== '' ? {uri: item.uri, width: 100, height: 100} : {};
+        const uri = item.uri !== '' ? {uri: item.uri, width: 200, height: 200} : {};
         return(
-            <View>
-                {item.uri !== '' && <Image source={uri}/>}
+            <View style={styles.container} key={item.key}>
+                <View style={styles.image}>
+                    {item.uri !== '' && <Image source={uri}/>}
+                </View>
                 {this.state.data !== [] &&
                     <FlatList
-                       data={this.state.data}
-                        renderItem={({item}) => <Questionary question={item} />}
+                       data={this.state.data[0]}
+                       contentContainerStyle={{paddingBottom: 20}}
+                       renderItem={({item}) => <Questionary question={item} />}
+                       ListHeaderComponent={() => <View style={styles.divider}/>}
+                       ListFooterComponent={() => <View style={styles.divider}/>}
                    />}
             </View>
         )
